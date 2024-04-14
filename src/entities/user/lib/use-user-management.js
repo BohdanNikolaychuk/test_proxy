6b886c3d093ba86028d743a3042c65dcb32a8d26
user <=katynskyiantonwork@gmail.com>
2024-04-14 10:45:01 +0300

    feat:optimzie hook

--- src/entities/user/lib/use-user-management.js
+++ src/entities/user/lib/use-user-management.js
@@ -1,17 +1,24 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 export const useUserManagement = usersData => {
   const [searchValue, setSearchValue] = useState('');
   const [sortOrder, setSortOrder] = useState('asc');
 
-  const filteredUsers = usersData.filter(user =>
-    user.username.toLowerCase().includes(searchValue.toLowerCase())
+  const filteredUsers = useMemo(
+    () =>
+      usersData.filter(user =>
+        user.username.toLowerCase().includes(searchValue.toLowerCase())
+      ),
+    [usersData, searchValue]
   );
 
-  const sortedUsers =
-    sortOrder === 'asc'
-      ? filteredUsers.sort((a, b) => a.username.localeCompare(b.username))
-      : filteredUsers.sort((a, b) => b.username.localeCompare(a.username));
+  const sortedUsers = useMemo(() => {
+    const comparator =
+      sortOrder === 'asc'
+        ? (a, b) => a.username.localeCompare(b.username)
+        : (a, b) => b.username.localeCompare(a.username);
+    return [...filteredUsers].sort(comparator);
+  }, [filteredUsers, sortOrder]);
 
   const handleSearchChange = value => {
     setSearchValue(value);
